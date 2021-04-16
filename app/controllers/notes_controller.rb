82dca830301daf9619ef8ff5594391ed3c735eb4
KevJSDevelopment <Kshhn12@gmail.com>
2021-04-16 09:38:04 -0400

    controller actions added

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,27 @@
 class NotesController < ApplicationController
+
+    def new
+        render :login_form
+    end
+
+    def create
+
+    end
+
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    def update
+
+    end
+
+    def destroy
+
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,15 @@
 class UsersController < ApplicationController
+
+    def create
+
+    end
+
+    def login
+
+    end
+
+    def logout
+
+    end
+    
 end
