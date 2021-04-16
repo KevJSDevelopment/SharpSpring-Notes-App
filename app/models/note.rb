fb8423ff8968adb6a458dd33d32e36ad33045353
KevJSDevelopment <Kshhn12@gmail.com>
2021-04-16 09:11:52 -0400

    validations working, relationships for models created

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,5 +1,5 @@
 class Note < ApplicationRecord
     belongs_to :user
-    validates :title, presence: true, length: { maximum: 30, minimum: 1}
+    validates :title, presence: true, length: { maximum: 30 }
     validates :description, length: { maximum: 1000 }
 end
