e5f7db8d215cbb15cba4488e96040c9e91f2fa52
KevJSDevelopment <Kshhn12@gmail.com>
2021-04-20 18:42:16 -0400

    updated time on card

--- config/routes.rb
+++ config/routes.rb
@@ -8,12 +8,17 @@ Rails.application.routes.draw do
   get "/profile", to: "users#show", as: "profile"
   post "/logout", to: "users#logout", as: "logout"
   post "/edit", to: "notes#edit", as: "edit_note"
-  delete "/", to: "notes#destroy", as: "destroy_note"
+  delete "/note", to: "notes#destroy", as: "destroy_note"
 
   get "*path", to: redirect("/profile"), constraints: lambda { |request|
     request.session[:user_id]
   }
   get "*path", to: redirect('/login')
+
+  get "/", to: redirect("/profile"), constraints: lambda { |request|
+    request.session[:user_id]
+  }
+  get "/", to: redirect('/login')
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
